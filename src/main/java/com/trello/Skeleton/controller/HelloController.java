1947780ac7a8aea629cb8b37c430afc7df4b4eeb
Illia Kuznetsov <kyzya.illia@gmail.com>
2022-07-19 18:52:58 +0200

    Initial commit #2

--- src/main/java/com/trello/Skeleton/controller/HelloController.java
+++ src/main/java/com/trello/Skeleton/controller/HelloController.java
@@ -1,11 +1,12 @@
 package com.trello.Skeleton.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
+    @GetMapping("/hello")
     public String getHello() {
         return "Hello";
     }
